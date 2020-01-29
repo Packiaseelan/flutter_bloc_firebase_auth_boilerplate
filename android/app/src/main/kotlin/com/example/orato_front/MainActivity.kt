b3d66b8a96d390613c5a7168f5b799d4865043af
axel COUDRAY <axel.hpm.coudray@gmail.com>
2020-01-29 09:30:41 +0100

    flutter firebase imp ios

--- android/app/src/main/kotlin/com/example/orato_front/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/orato_front/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_bloc_firebase_auth_boilerplate
+package com.example.flutter-bloc-auth-boiler
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
